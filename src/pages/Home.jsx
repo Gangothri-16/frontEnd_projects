26015e0dc0fec70d2356fd51207668cc17b419f8
Gangothri-16 <gangothripettam@gmail.com>
2025-02-13 13:41:31 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Style.css'
+export default function Home() {
+  return (
+    <div>
+      <h1>TO-DO-APP</h1>
+      <div id='container'>
+      <textarea  id="t1" placeholder='Enter task'></textarea>
+      <button id="button">ADD TASK</button>
+      </div>
+    </div>
+    
+    
+  )
+}
